03dfdf39cd65bc72c253ea81b3fe5451e753ef5a
Byron Varberg <byron@crema.us>
2020-03-06 13:28:53 -0600

    Generate ButtonCreateNewTask and add test stub
    Focused on what this thing will need to do.

---
+++ src/components/ButtonCreateNewTask/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+interface Props {}
+
+export function ButtonCreateNewTask({}: Props) {
+  return null
+}
---
+++ src/components/ButtonCreateNewTask/test.tsx
@@ -0,0 +1,7 @@
+import { fireEvent, render } from "@testing-library/react"
+import React from "react"
+import { ButtonCreateNewTask } from "."
+
+describe("ButtonCreateNewTask", () => {
+  it.todo("calls the provided click event handler when clicked")
+})
