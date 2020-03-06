b497b9542c6514a7f7be78ba101d96b9c43dfde1
Byron Varberg <byron@crema.us>
2020-03-06 14:44:23 -0600

    Add first test implementation

--- src/components/ActionCreateNewTask/test.tsx
+++ src/components/ActionCreateNewTask/test.tsx
@@ -1,9 +1,17 @@
-import { fireEvent, render } from "@testing-library/react"
+import { cleanup, fireEvent, render } from "@testing-library/react"
 import React from "react"
 import { ActionCreateNewTask } from "."
 
 describe("ActionCreateNewTask", () => {
-  it.todo("clicking 'create new task' reveals a form for creating a task")
+  afterEach(cleanup)
+
+  it("clicking 'Create New Task' reveals a form for creating a task", () => {
+    const { getByText, getByPlaceholderText } = render(<ActionCreateNewTask />)
+    fireEvent.click(getByText("Create New Task"))
+    const input = getByPlaceholderText("Task description")
+
+    expect(input).toBeDefined()
+  })
 
   describe("when the user types a task description and submits the form", () => {
     it.todo("stores the task")
