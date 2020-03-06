189f9d908dcba08368f451f0ed1078fa1896e166
Byron Varberg <byron@crema.us>
2020-03-06 13:30:39 -0600

    Start with an assertion

--- src/components/ButtonCreateNewTask/test.tsx
+++ src/components/ButtonCreateNewTask/test.tsx
@@ -1,7 +1,10 @@
 import { fireEvent, render } from "@testing-library/react"
 import React from "react"
+import testdouble from "testdouble"
 import { ButtonCreateNewTask } from "."
 
 describe("ButtonCreateNewTask", () => {
-  it.todo("calls the provided click event handler when clicked")
+  it("calls the provided click event handler when clicked", () => {
+    testdouble.verify(mockOnClick())
+  })
 })
