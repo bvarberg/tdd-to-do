76bd7c71e712b279faa0a3849b59fdf8ffebec14
Byron Varberg <byron@crema.us>
2020-03-06 18:30:57 -0600

    Implement component in test file
    Avoiding the need to import/export a bunch of stuff just to test the
    design.

--- src/components/ActionCreateNewTask/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-interface Props {}
-
-export function ActionCreateNewTask({}: Props) {
-  return null
-}
--- src/components/ActionCreateNewTask/test.tsx
+++ src/components/ActionCreateNewTask/test.tsx
@@ -1,6 +1,38 @@
 import { cleanup, fireEvent, render } from "@testing-library/react"
-import React from "react"
-import { ActionCreateNewTask } from "."
+import React, { createContext, useContext, useState } from "react"
+import testdouble from "testdouble"
+
+interface TaskStorage {
+  insert(opts: { description: string }): Promise<void>
+}
+
+const defaultTaskStorage = testdouble.object<TaskStorage>()
+const ContextTaskStorage = createContext<TaskStorage>(defaultTaskStorage)
+
+function ActionCreateNewTask() {
+  const taskStorage = useContext(ContextTaskStorage)
+
+  const [description, setDescription] = useState<string>("")
+  const handleDescriptionChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    setDescription(event.target.value)
+  }
+
+  const handleClickSave = () => {
+    taskStorage.insert({ description })
+  }
+
+  return (
+    <div>
+      <input
+        placeholder="Task description"
+        value={description}
+        onChange={handleDescriptionChange}
+      />
+      <button onClick={handleClickSave}>Save</button>
+      <button>Create New Task</button>
+    </div>
+  )
+}
 
 describe("ActionCreateNewTask", () => {
   afterEach(cleanup)
@@ -13,7 +45,24 @@ describe("ActionCreateNewTask", () => {
     expect(input).toBeDefined()
   })
 
-  describe("when the user types a task description and submits the form", () => {
-    it.todo("stores the task")
+  describe("when the user types a task description and clicks save", () => {
+    it("inserts a new task in task storage", () => {
+      const taskStorage = testdouble.object<TaskStorage>()
+
+      const { getByText, getByPlaceholderText } = render(
+        <ContextTaskStorage.Provider value={taskStorage}>
+          <ActionCreateNewTask />,
+        </ContextTaskStorage.Provider>,
+      )
+      fireEvent.click(getByText("Create New Task"))
+      fireEvent.change(getByPlaceholderText("Task description"), {
+        target: {
+          value: "My first task",
+        },
+      })
+      fireEvent.click(getByText("Save"))
+
+      testdouble.verify(taskStorage.insert({ description: "My first task" }))
+    })
   })
 })
